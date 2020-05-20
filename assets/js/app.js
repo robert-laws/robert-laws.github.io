8ef052c399acd2a3f187099e1d218ed45ce92f71
Robert Laws <roblaws@me.com>
2020-05-20 12:06:25 +0300

    add highlight for focused scrolled nav in project pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,36 @@ navbarToggle.addEventListener('click', function () {
 });
 
 
+// console.log($('.project-detail__text h2'));
+// console.log($('.project-detail__text h2').offset());
+
+
+
+
+$(window).scroll(function (event) {
+  var scroll = $(window).scrollTop();
+  var latestEl = -1;
+  $('.project-detail__text h2').each(function(index) {
+    if(($(this).offset().top - 10) <= scroll) {
+      latestEl = index
+    }
+    // console.log($(this).offset().top);
+  });
+
+  if(latestEl == -1) {
+    $('.project-detail__side-nav a[href*="#"]').removeClass('textBold');
+  } else {
+    $('.project-detail__side-nav a[href*="#"]').each(function(index) {
+      if(latestEl == index) {
+        $('.project-detail__side-nav a[href*="#"]').eq(index).addClass('textBold')
+      } else if(latestEl != index) {
+        $('.project-detail__side-nav a[href*="#"]').eq(index).removeClass('textBold');
+      }
+    })
+  }
+  // console.log(latestEl);
+});
+
 
 // Project page - side-nav smooth scrolling
 // Select all links with hashes
@@ -22,6 +52,12 @@ $('a[href*="#"]')
   .not('[href="#"]')
   .not('[href="#0"]')
   .click(function (event) {
+    // finds all links with a # and removes the textBold class
+    // $('a[href*="#"]').removeClass('textBold');
+
+    // adds the 'textBold' class to the scroll link clicked -> making it bold
+    // $(event.target).addClass('textBold')
+    
     // On-page links
     if (
       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
@@ -34,7 +70,7 @@ $('a[href*="#"]')
       if (target.length) {
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
-        $('html, body').animate({
+        $('html, body').animate({          
           scrollTop: target.offset().top
         }, 1000, function () {
           // Callback after animation
